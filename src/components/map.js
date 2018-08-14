41d93d738aa258946054dd768bd2adcaab9ea85d
cveiga819 <cveiga819@gmail.com>
2018-08-14 10:52:49 +0100

    issue #7 fix!

--- src/components/map.js
+++ src/components/map.js
@@ -2,23 +2,31 @@ import React, { Component } from 'react';
 import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 
 class MapContainer extends Component {
-  render() {
-    //create the array with markers(empty)
-    const markers = this.props.markers || [];
 
-    //create the style for the markers
-    const icon = {
-      url:'https://github.com/cveiga819/assets/blob/master/markers.png?raw=true'
-    }
+  //TODO: create the info windows 
+  
+  render() {
+    //create the array with markers
+    const marker = this.props.markers.map((venue, i) => {
+      // set the location similar to the foursquare points
+      const marker = {
+        position: {
+          lat: venue.location.lat,
+          lng: venue.location.lng
+        }
+      }
+      //set the icon for the markers
+      const icon = {
+        url:'https://github.com/cveiga819/assets/blob/master/markers_1.png?raw=true'
+      }
+      return <Marker key={i} icon={icon} {...marker} />
+    });  
 
     return (
       <GoogleMap
         defaultZoom={13}
         defaultCenter={this.props.center}>
-        {markers.map((marker, i) => (
-            <Marker icon={icon} key={i} {...marker}  />
-          )
-        )}
+        {marker}
       </GoogleMap>
     );
   }
