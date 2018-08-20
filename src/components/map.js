f2465073e3f02a45bc0a65fd0c509f914c2d4d91
cveiga819 <cveiga819@gmail.com>
2018-08-20 12:47:40 +0100

    adding infowindows in the map

--- src/components/map.js
+++ src/components/map.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import { withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps";
 
 class MapContainer extends Component {
+  //constructor
+  constructor(props) {
+    super(props);
+    this.state ={
+      venue: [],
+      hasError: false,
+      isOpen: false
+    }
+  }
 
-  //TODO: create the info windows 
+  //Adding the infoWindows open
+  toggleInfoWindow = venue => {
+    this.setState({ isOpen: !this.state.isOpen });
+  } 
+
+  //Catching Error with Component did Catch, tutorial followed by the React Documentation
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+  }
   
   render() {
+    //error handling
+    if(this.state.hasError) {
+      return(
+        <div className="Error_box">
+          <h1>Ops, we are sorry, but something went wrong</h1>
+          <p>Please check your internet connection, and try again.</p>
+        </div>
+      )
+    }
     //create the array with markers
     const marker = this.props.markers.map((venue, i) => {
       // set the location similar to the foursquare points
@@ -19,7 +45,12 @@ class MapContainer extends Component {
       const icon = {
         url:'https://github.com/cveiga819/assets/blob/master/markers_1.png?raw=true'
       }
-      return <Marker key={i} icon={icon} animation={window.google.maps.Animation.DROP} {...marker} />
+      return <Marker key={i} icon={icon} animation={window.google.maps.Animation.DROP} onClick={() => this.toggleInfoWindow(venue)} {...marker}>
+              {this.state.isOpen && 
+              <InfoWindow onCloseClick={() => this.toggleInfoWindow(venue)}>
+                <h5>{venue.name}</h5>
+              </InfoWindow>}
+            </Marker>
     });  
 
     return (
